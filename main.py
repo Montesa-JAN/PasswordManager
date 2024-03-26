3c569723bd07f04085576de46c821e1371723ee4
Montesa-JAN <105424880+Montesa-JAN@users.noreply.github.com>
2024-03-26 23:40:10 +0800

    Changed directory of "logo.png"

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ window.config(padx=50, pady=50)
 
 # Canvas
 canvas = Canvas(width=200, height=200)
-logo_img = PhotoImage(file="logo.png")
+logo_img = PhotoImage(file="./images/logo.png")
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=2)
 
